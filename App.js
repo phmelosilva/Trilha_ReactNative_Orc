7765b0e656c336ce7fc3f2c3d2b691060d0e1b08
Pedro Henrique <pedrodsm1819@gmail.com>
2023-10-10 22:03:12 -0300

    Configurando primeiro desafio e stacks

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import desafio1 from './src/views/desafio1';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Carteira de Uber" screenOptions={{headerShown:false}} >
+        <Stack.Screen name="Carteira de Uber" component={desafio1} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/views/desafio1/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const desafio1 = () => {
+	return (
+		<View>
+			<Text> Oi Daniel </Text>
+		</View>  	
+  );
+}
+
+export default desafio1;
