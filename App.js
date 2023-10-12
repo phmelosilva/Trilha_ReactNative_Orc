9c8cb58e81c7d4fa1a2ea7fc3f1bc7de285754f6
Pedro Henrique <pedrodsm1819@gmail.com>
2023-10-12 00:53:20 -0300

    Descrição da Carteira de Uber

--- App.js
+++ App.js
@@ -1,29 +1,11 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import desafio1 from './pages/desafio1';
-import React, { useState } from 'react';
-import * as Font from 'expo-font';
-import AppLoading from 'expo-app-loading';
+import React, { useState, useEffect } from 'react';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-
-  const [dummy, setDummy] = useState(false);
-
-  Font.loadAsync({
-    'sourcecodepror':require('./src/assets/fonts/SourceCodePro-Regular.ttf'),
-    'sourcecodeprob':require('./src/assets/fonts/SourceCodePro-Bold.ttf')
-
-  }).then(() => {
-    setDummy(true);
-  })
-
-  if(!dummy){
-    return(
-      <AppLoading />
-    )
-  }
 
   return (
     <NavigationContainer>
--- pages/desafio1/index.js
+++ pages/desafio1/index.js
@@ -5,17 +5,65 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const carteira_digital = () => {
 	return (
-		<View>
-			<Text style={styles.titulo} > Carteira Digital </Text>
-		</View>  	
+		<View style={styles.container}>
+			<View >
+				<Text style={styles.titulo} > Carteira Digital Roubada </Text>
+			</View>
+
+			<View>
+				<Text style={styles.textoNeg} > Foto do Meliante </Text>
+			</View>
+
+			<View> 
+				<Text>
+					<Text style={styles.textoNeg} > Nome: </Text>
+					<Text> Morb Brown </Text>
+				</Text>
+				<Text>
+					<Text style={styles.textoNeg} > Habilitação: </Text> 
+					<Text> Krai </Text>
+				</Text>
+				<Text>
+					<Text style={styles.textoNeg} > Validade: </Text> 
+					<Text> enquanto eu puder espionar sua mãe </Text>
+				</Text>
+				<Text>
+					<Text style={styles.textoNeg} > Identidade: </Text> 
+					<Text> O cara do fusca amarelo que ganha do Kwid no racha </Text>
+				</Text>
+			</View>
+
+			<View>
+				<Text>
+					Imprima seu motorista de excelência
+				</Text>
+
+				<Text>
+					Perca sua mãe, DE NOVO :D
+				</Text>
+			</View>
+
+		</View>  		
   );
 }
 
 const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		top: "7%",
+		color: "red",
+	},
+
 	titulo: {
 		fontFamily: 'sourcecodepror',
 		fontSize: 30,
 	},
+	
+	textoNeg: {
+		fontWeight: "bold",
+	},
+
 });
 
 export default carteira_digital;
