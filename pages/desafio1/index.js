181b44f7d73d07159dcf361424de0001ddf927d6
Pedro Henrique <pedrodsm1819@gmail.com>
2023-10-12 15:27:42 -0300

    Adicionando imagens

--- pages/desafio1/index.js
+++ pages/desafio1/index.js
@@ -1,23 +1,33 @@
 import React  from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 
 // import { Container } from './styles';
 
 const carteira_digital = () => {
 	return (
 		<View style={styles.container}>
+			<Image
+					style={styles.fundo} source={require('./src/assets/imgs/fundo_carteira.jpeg')}
+			/>
+
 			<View >
-				<Text style={styles.titulo} > Carteira Digital Roubada </Text>
+				<Text style={styles.titulo} > Carteira de Habilitação Roubada </Text>
+				<Image
+					style={styles.foto} source={require('./src/assets/imgs/malbec.jpg')}
+				/>
 			</View>
 
 			<View>
 				<Text style={styles.textoNeg} > Foto do Meliante </Text>
+				<Image
+					style={styles.foto} source={require('./src/assets/imgs/malbeck.jpeg')}
+				/>
 			</View>
 
-			<View> 
+			<View > 
 				<Text>
 					<Text style={styles.textoNeg} > Nome: </Text>
-					<Text> Morb Brown </Text>
+					<Text> Morb Brown (/•ิ_•ิ)/ </Text>
 				</Text>
 				<Text>
 					<Text style={styles.textoNeg} > Habilitação: </Text> 
@@ -34,13 +44,15 @@ const carteira_digital = () => {
 			</View>
 
 			<View>
-				<Text>
-					Imprima seu motorista de excelência
-				</Text>
-
-				<Text>
-					Perca sua mãe, DE NOVO :D
-				</Text>
+				<Button
+					title="Imprima seu motorista de excelência"
+					onPress={() => Alert.alert('ótima escolha')}
+				/>
+				
+				<Button
+					title="Percua sua mãe, DE NOVO :D"
+					onPress={() => Alert.alert('Perdeu parça')}
+				/>
 			</View>
 
 		</View>  		
@@ -56,10 +68,22 @@ const styles = StyleSheet.create({
 	},
 
 	titulo: {
-		fontFamily: 'sourcecodepror',
 		fontSize: 30,
+
+	},
+
+	foto: {
+		borderRadius: 40,
+    	borderWidth: 2,
+		borderColor: 'white'
+	},
+
+	fundo: {
+		zIndex: 1,
 	},
 	
+
+
 	textoNeg: {
 		fontWeight: "bold",
 	},
